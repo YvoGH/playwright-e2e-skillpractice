06e32ced655d189ec521488054f24f295c804da5
Adam <awiejak@int.pl>
2026-08-21 20:18:06 +0200

    Zmiany POM

--- tests/03-nauka-POM.ts
+++ tests/03-nauka-POM.ts
@@ -1,30 +1,26 @@
-import { test, expect, Page } from '@playwright/test';
+import { test, expect, Page, Locator } from '@playwright/test';
 export class LoginPage {
     readonly page: Page;
+    readonly emailInput: Locator;
+    readonly hasloInput: Locator;
+    readonly zalogujButton: Locator;
+
     constructor(page: Page) {
         this.page = page;
+        this.emailInput = page.getByPlaceholder('Wprowadź e-mail');
+        this.hasloInput = page.getByPlaceholder('Wprowadź hasło');
+        this.zalogujButton = page.getByRole('button', { name: 'Zaloguj się'});
+
     }
     async zalogujUzytkownika(email: string, haslo: string){
-        await this.page.getByPlaceholder('Wprowadź e-mail').fill(email);
-        await this.page.getByPlaceholder('Wprowadź hasło').fill(haslo);
-        await this.page.getByRole('button', { name: 'Zaloguj się'}).click();
+        await this.emailInput.fill(email);
+        await this.hasloInput.fill(haslo);
+        await this.zalogujButton.click();
     }
 }
-
 test('Logowanie z POM', async ({ page }) => {
     await page.goto('https://app.example.com/login');
     const logowanie = new LoginPage(page);
     await logowanie.zalogujUzytkownika('qa@example.com', 'Haslo123!');
     await expect(page).toHaveURL('https://app.example.com/dashboard');
-});
-
-test('Pomyślne logowanie użytkownika', async ({ page }) => {
-    await page.goto('https://app.example.com/login');
-    await page.getByPlaceholder('Wprowadź e-mail').fill('qa@example.com');
-    await page.getByPlaceholder('Wprowadź hasło').fill('Haslo123!');
-    await page.getByRole('button', { name: 'Zaloguj się' }).click();
-    await expect(page).toHaveURL('https://app.example.com/dashboard');
-
-    const headingMessage = page.getByRole('heading', { name: 'Witaj w panelu'});
-    await expect(headingMessage).toBeVisible();
 });
