93523e20f3077f74d71ea99bf6f15dbcf6c63d49
Adam <awiejak@int.pl>
2026-08-26 20:05:11 +0200

    Beforeeach and hooks refactory

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,23 +1,26 @@
 import { test, expect } from "@playwright/test";
 import {LoginPage} from "../src/pages/LoginPage";
 
+let logowanie: LoginPage;
+test.beforeEach(async ({ page }) => {
+   logowanie = new LoginPage(page);
+   await page.goto('https://app.example.com/login');
+});
+
 const daneLogowania = [
     { email: 'qa@example.com', haslo: 'Haslo123!', oczekiwanyUrl: 'https://app.example.com/dashboard'},
     { email: 'zly@example.com', haslo: 'Haslo123!', oczekiwanyUrl: 'https://app.example.com/login'},
     { email: 'qa@example.com', haslo: 'ZleHaslo!', oczekiwanyUrl: 'https://app.example.com/login'},
 ];
+
 for (const dane of daneLogowania) {
     test(`Logowanie dla użytkownika: ${dane.email} i hasła: ${dane.haslo}`, async ({page}) => {
-        await page.goto('https://app.example.com/login');
-        const logowanie = new LoginPage(page);
         await logowanie.zalogujUzytkownika(dane.email, dane.haslo);
         await expect(page).toHaveURL(dane.oczekiwanyUrl);
     });
 }
 
 test('Nieudane logowanie - niepoprawne hasło', async ({ page }) => {
-    await page.goto('https://app.example.com/login');
-    const logowanie = new LoginPage(page);
     await logowanie.zalogujUzytkownika('qa@example.com', 'ZleHaslo123');
     await logowanie.sprawdzKomunikatBledu('Nieprawidłowy login lub hasło');
 });
