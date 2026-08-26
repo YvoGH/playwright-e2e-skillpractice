3df37542a8a20f1fd09c06afac31c7bcbb03d2dd
Adam <awiejak@int.pl>
2026-08-26 19:34:24 +0200

    refactor: implement Data-driven-test

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,9 +1,16 @@
 import { test, expect } from "@playwright/test";
 import {LoginPage} from "../src/pages/LoginPage";
 
-test('Logowanie z POM', async ({ page }) => {
-    await page.goto('https://app.example.com/login');
-    const logowanie = new LoginPage(page);
-    await logowanie.zalogujUzytkownika('qa@example.com', 'Haslo123!');
-    await expect(page).toHaveURL('https://app.example.com/dashboard');
-});
+const daneLogowania = [
+    { email: 'qa@example.com', haslo: 'Haslo123!', oczekiwanyUrl: 'https://app.example.com/dashboard'},
+    { email: 'zly@example.com', haslo: 'Haslo123!', oczekiwanyUrl: 'https://app.example.com/login'},
+    { email: 'qa@example.com', haslo: 'ZleHaslo!', oczekiwanyUrl: 'https://app.example.com/login'},
+];
+for (const dane of daneLogowania) {
+    test(`Logowanie dla użytkownika: ${dane.email} i hasła: ${dane.haslo}`, async ({page}) => {
+        await page.goto('https://app.example.com/login');
+        const logowanie = new LoginPage(page);
+        await logowanie.zalogujUzytkownika(dane.email, dane.haslo);
+        await expect(page).toHaveURL(dane.oczekiwanyUrl);
+    });
+}
